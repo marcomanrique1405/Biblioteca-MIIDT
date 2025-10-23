ce334c6385ff9b694457f24224613c908a94aaa8
Marco <mancomencho83@gmail.com>
2025-10-23 11:05:15 -0600

    Instalacion de Bootstrap en el proyecto

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/stile.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="body-wrap">
+        <main>
+            <div class="container py-4">
+            <h1 class="text-primary">Bootstrap OK 🎉</h1>
+            <button class="btn btn-success">Probar</button>
+            </div>
+        </main>
+    </div>
+
+<script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
